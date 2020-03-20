7c7b8e5622a947f041f0ba7a4272be7db918032c
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-20 14:49:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,37 +1,40 @@
 class Transfer
-attr_accessor :status, :balance
-attr_reader :sender, :name, :amount, :receiver
+  attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
-    @sender = sender
-    @name = name
-    @amount = amount
-    @status = "pending"
-    @receiver = receiver
-    @balance = balance
-    @transactions = []
+    @status = "pending" #an instance of the Transfer class initializes with a staus of "pending"
+    @sender = sender #an instance of the Transfer class initializes with a sender
+    @receiver = receiver #an instnace of the Transfer class intitializes with a reciever
+    @amount = amount #an instance of the Transfer class initializes with a an amount
   end
 
-  def valid?
-    @sender.valid? && @receiver.valid?
-  end
-
-  def execute_transaction
-    if @sender.balance >= @amount && @status != "complete"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    elsif @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+  #can check that both accounts are valid & uses #valid? from BankAccount class
+  def valid? 
+    if self.sender.valid? == true && self.receiver.valid? == true && sender.balance >= self.amount
+      true
+    else
+      false
     end
   end
 
+  #can execute a successful transaction between two accounts
+  def execute_transaction
+    if self.valid? == true && self.status == "pending" #each transfer can only happen once
+      receiver.balance += self.amount
+      sender.balance -= self.amount
+      self.status = "complete" #each transfer can only happen once
+    else #rejects a transfer if the sender doesn't have a valid account
+      self.status ="rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  #can reverse a transfer between two accounts
   def reverse_transfer
-    if @sender.balance >= @amount && @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
+    if self.status == "complete" #it can only reverse executed transfers
+      receiver.balance -= self.amount
+      sender.balance += self.amount
+      self.status = "reversed"
     end
   end
 
